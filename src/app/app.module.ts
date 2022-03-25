79171fda1667745f666923b70bd775cdd0cb07d5
Michael Sirio <msirio@extron.com>
2022-03-25 16:50:03 -0400

    probably incorrect button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SimpleButtonComponent } from './library/simple-button/simple-button.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SimpleButtonComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/library/simple-button/simple-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-simple-button",
+  templateUrl: "./simple-button.component.html",
+  styleUrls: ["./simple-button.component.css"],
+})
+export class SimpleButtonComponent implements OnInit {
+  @Input() buttonText: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
